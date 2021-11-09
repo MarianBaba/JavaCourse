e9e289b024a45a91fae07858cfa55e39c1f079c6
MarianBaba <marianbaba16@gmail.com>
2021-11-09 17:40:46 +0100

    wrapper classes

---
+++ src/WrapperDemo.java
@@ -0,0 +1,12 @@
+
+
+public class WrapperDemo {
+    public static void main(String[] args) {
+        int i = 1;
+        Integer num = Integer.valueOf(i); //COSI' POSSIAMO USARE LE CLASSI WRAPPER, per ogni tipo primitivo c'è una classe wrapper
+
+        //Java a volte lo fa da solo, facendo AUTOBOXING
+
+
+    }
+}
